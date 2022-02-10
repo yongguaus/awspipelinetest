85c161ae424df2f6299e128aeed7d5fc3944a8d4
Yong Gu <yong.gu@vitalsoftware.net>
2022-02-10 17:12:01 +1100

    ff

--- pipe_line_test/pipe_line_test_stack.py
+++ pipe_line_test/pipe_line_test_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     Stack,
     # aws_sqs as sqs,
     aws_codepipeline,
+    pipelines,
 )
 from constructs import Construct
 
@@ -11,28 +12,16 @@ class PipeLineTestStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        pipeline = aws_codepipeline.PipeLine(
+        pipeline = pipelines.CodePipeline(
            scope=self,
            id="TestPipeline",
-           pipelinename="TestPipeline",
-           synth=ShellStep("Synth", 
-                            input=CodePipelineSource.git_hub("OWNER/REPO", "main"),
-                            commands=["npm install -g aws-cdk", 
+           synth=pipelines.ShellStep("Synth", 
+                            input=pipelines.CodePipelineSource.git_hub("yongguaus/awspipelinetest", "main"),
+                            commands=[
+                                "npm install -g aws-cdk", 
                                 "python -m pip install -r requirements.txt", 
                                 "cdk synth"]
                         )
            )
          
-        dev_stage = pipeline.add_stage(
-            stage_name="Dev",
-
-            
-            )
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PipeLineTestQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+       
