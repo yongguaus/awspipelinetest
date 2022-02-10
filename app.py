1b3b653fe031ad3cc49dadd9d20008447b7fc4d0
Yong Gu <yong.gu@vitalsoftware.net>
2022-02-10 15:05:19 +1100

    oo

--- app.py
+++ app.py
@@ -7,22 +7,8 @@ from pipe_line_test.pipe_line_test_stack import PipeLineTestStack
 
 
 app = cdk.App()
-PipeLineTestStack(app, "PipeLineTestStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+PipeLineTestStack(app, "PipeLineTestStack", 
+    env=cdk.Environment(account="889043514394", region="ap-southeast-2")
     )
 
 app.synth()
--- pipe_line_test/pipe_line_test_stack.py
+++ pipe_line_test/pipe_line_test_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_codepipeline,
 )
 from constructs import Construct
 
@@ -9,6 +10,24 @@ class PipeLineTestStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        pipeline = aws_codepipeline.PipeLine(
+           scope=self,
+           id="TestPipeline",
+           pipelinename="TestPipeline",
+           synth=ShellStep("Synth", 
+                            input=CodePipelineSource.git_hub("OWNER/REPO", "main"),
+                            commands=["npm install -g aws-cdk", 
+                                "python -m pip install -r requirements.txt", 
+                                "cdk synth"]
+                        )
+           )
+         
+        dev_stage = pipeline.add_stage(
+            stage_name="Dev",
+
+            
+            )
 
         # The code that defines your stack goes here
 
